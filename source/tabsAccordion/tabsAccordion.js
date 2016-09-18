f339a2ff358c7ed0b7906c2649b117440023b0cc
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-17 23:24:33 -0300

    add: config script

--- source/tabsAccordion/tabsAccordion.js
+++ source/tabsAccordion/tabsAccordion.js
@@ -1,8 +1,15 @@
 import './tabsAccordion.scss';
 
 class tabsAccordion {
-  constructor() {
-    console.log('alert')
+  constructor( options ) {
+    const config = {
+      link      : '.tabs_accordion-title',
+      content   : '.tabs_accordion-content',
+      itemClass : 'on'
+    };
+    const options = options || config;
+
+    console.log('alert');
   }
 }
 
