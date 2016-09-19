6442afda08a53c5267d1ab52b15683c7cd9d6b65
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-09-18 22:37:33 -0300

    update: initial script

--- source/tabsAccordion/tabsAccordion.js
+++ source/tabsAccordion/tabsAccordion.js
@@ -3,11 +3,43 @@ import './tabsAccordion.scss';
 class tabsAccordion {
   constructor( options ) {
     const config = {
-      link      : '.tabs_accordion-title',
-      content   : '.tabs_accordion-content',
-      itemClass : 'on'
+      container    : '.tabs_accordion',
+      itemsNav     : '.tabs_accordion-title',
+      itemsContent : '.tabs_accordion-content',
+      activeClass : 'on'
     };
     const options = options || config;
+    let heightContainer = 0;
+
+    document.querySelectorAll( options.itemsContent ).forEach(function(index, element){
+      element.style.position = 'relative';
+      if( index === 0 ){
+        heightContainer = element.innerHeight() + 20;
+      }
+      element.removeAttr('style');
+      element.classList.add('hide');
+    });
+
+    document.querySelector( container ).style.minHeight = heightContainer;
+
+    document.querySelector( itemsNav ).classList.add( options.activeClass );
+    document.querySelector( itemsContent ).classList.add('show');
+
+    document.querySelectorAll( options.itemsContent ).forEach(function(index, element){
+      element.addEventListener('click', function(event){
+        event.preventDefault();
+
+        // if( !$(this).hasClass( opcoes.classe_abas )){
+        //   $( opcoes.link_abas ).removeClass( opcoes.classe_abas );
+        //   $( opcoes.conteudo_abas ).removeClass( 'show' );
+
+        //   $('.tabs_accordion').css('min-height', $(this).next( opcoes.conteudo_abas ).height() + 20 );
+
+        //   $(this).addClass( opcoes.classe_abas );
+        //   $(this).next( opcoes.conteudo_abas ).addClass( 'show' );
+        // }
+      });
+    });
 
     console.log('alert');
   }
